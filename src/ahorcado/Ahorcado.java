5ab1a65389052b708eacf74f4a4df2183ac5ae4d
maitte <maitena_escalada@hotmail.com>
2022-03-19 22:24:00 -0300

    Modificacion convertirPalabraAGuiones

--- src/ahorcado/Ahorcado.java
+++ src/ahorcado/Ahorcado.java
@@ -5,13 +5,12 @@ import java.util.Random;
 public class Ahorcado {
 	private String[] palabras = {"hola","casa","auto","perro"};
 	private String palabra;
-	private String palabraConGuiones;
+	private char[] palabraConGuiones;
 	private int puntaje;
 	private int intentos;
 	
 	public Ahorcado(int intentos) {
 		this.palabra = "";
-		this.palabraConGuiones = "";
 		this.intentos = intentos; 
 		this.puntaje = 0;
 	}
@@ -22,9 +21,11 @@ public class Ahorcado {
 		return this.palabra = this.palabras[elem];
 	}
 
-	public String convertirPalabraAGuiones(String palabra) {
+	public char[] convertirPalabraAGuiones(String palabra) {
+		this.palabraConGuiones = new char[palabra.length()];
+	
 		for (int i = 0; i < palabra.length(); i++) {
-			this.palabraConGuiones += "-";
+			this.palabraConGuiones[i] = '-';
 		}
 		return this.palabraConGuiones;
 	}
