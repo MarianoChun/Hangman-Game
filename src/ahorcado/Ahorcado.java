18eb5e997106bb468a10a8548b626824974bf106
maitte <maitena_escalada@hotmail.com>
2022-03-22 11:38:32 -0300

    ADD botones en interfaz

--- src/ahorcado/Ahorcado.java
+++ src/ahorcado/Ahorcado.java
@@ -3,41 +3,42 @@ package ahorcado;
 import java.util.Random;
 
 public class Ahorcado {
-	private String[] palabras = {"hola","casa","auto","perro"};
+	private String[] palabras = { "hola", "casa", "auto", "perro" };
 	private String palabra;
 	private char[] palabraConGuiones;
 	private int puntaje;
 	private int intentos;
-	
-	// Podriamos colocar un valor por default para intentos y elegir la palabra directamente desde el constructor
-	// De esta forma, quedaria el constructor sin parametros 
+
+	// Podriamos colocar un valor por default para intentos y elegir la palabra
+	// directamente desde el constructor
+	// De esta forma, quedaria el constructor sin parametros
 	public Ahorcado() {
 		this.palabra = elegirPalabra();
-		this.palabraConGuiones = convertirPalabraAGuiones(palabra); 
-		this.intentos = 6; 
+		this.palabraConGuiones = convertirPalabraAGuiones(palabra);
+		this.intentos = 6;
 		this.puntaje = 0;
 	}
-	
+
 	public Ahorcado(String palabra) {
 		this.palabra = palabra;
-		this.palabraConGuiones = convertirPalabraAGuiones(palabra); 
-		this.intentos = 6; 
+		this.palabraConGuiones = convertirPalabraAGuiones(palabra);
+		this.intentos = 6;
 		this.puntaje = 0;
 	}
-	
-	public char[] obtenerPalabraAAdivinar(){
+
+	public char[] obtenerPalabraAAdivinar() {
 		return this.palabraConGuiones;
 	}
-	
+
 	public void adivinarLetra(char letra) {
-		if(adivinoLetra(letra)) {
+		if (adivinoLetra(letra)) {
 			cambiarEstadoPalabra(letra);
 			sumarPuntaje();
 		} else {
 			quitarIntentos();
 		}
 	}
-	
+
 	private String elegirPalabra() {
 		Random random = new Random();
 		int elem = random.nextInt(this.palabras.length);
@@ -46,29 +47,29 @@ public class Ahorcado {
 
 	private char[] convertirPalabraAGuiones(String palabra) {
 		this.palabraConGuiones = new char[palabra.length()];
-	
+
 		for (int i = 0; i < palabra.length(); i++) {
 			this.palabraConGuiones[i] = '-';
 		}
 		return this.palabraConGuiones;
 	}
-	
+
 	private void cambiarEstadoPalabra(char letra) {
-		for(int indice = 0; indice < palabraConGuiones.length;indice++) {
-			if(letra == palabra.charAt(indice)) {
+		for (int indice = 0; indice < palabraConGuiones.length; indice++) {
+			if (letra == palabra.charAt(indice)) {
 				palabraConGuiones[indice] = letra;
 			}
 		}
 	}
-	
+
 	private boolean adivinoLetra(char letra) {
-		return palabra.contains(""+letra);
+		return palabra.contains("" + letra);
 	}
-	
+
 	private int sumarPuntaje() {
 		return this.puntaje++;
 	}
-	
+
 	private int quitarIntentos() {
 		return this.intentos--;
 	}
@@ -80,5 +81,5 @@ public class Ahorcado {
 	public int getIntentos() {
 		return intentos;
 	}
-	
+
 }
--- src/test/AhorcadoTest.java
+++ src/test/AhorcadoTest.java
@@ -8,36 +8,35 @@ import ahorcado.Ahorcado;
 public class AhorcadoTest {
 	Ahorcado a;
 	String palabra;
-	
+
 	@Before
 	public void setUp() {
 		a = new Ahorcado("casa");
 	}
-	
-	
+
 	@Test
 	public void adivinarLetraTest() {
 		a.adivinarLetra('a');
-		char[] arrayEsperado = {'-','a','-','a'};
-		
-		assertArrayEquals(arrayEsperado,a.obtenerPalabraAAdivinar());
+		char[] arrayEsperado = { '-', 'a', '-', 'a' };
+
+		assertArrayEquals(arrayEsperado, a.obtenerPalabraAAdivinar());
 	}
-	
+
 	@Test
 	public void puntajeTest() {
 		a.adivinarLetra('a');
-		assertEquals(1,a.getPuntaje());
+		assertEquals(1, a.getPuntaje());
 	}
-	
+
 	@Test
 	public void intentoTest() {
 		a.adivinarLetra('a');
-		assertEquals(6,a.getIntentos());
+		assertEquals(6, a.getIntentos());
 	}
-	
+
 	@Test
 	public void restarIntentoTest() {
 		a.adivinarLetra('b');
-		assertEquals(5,a.getIntentos());
+		assertEquals(5, a.getIntentos());
 	}
 }
